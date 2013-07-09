0a3158ab3fd5eba14b641bae6d292d44da2dc4af
Rendaw <rendaw@zarbosoft.com>
2013-07-09 18:39:28 +0900

    Alternate updated website text.

--- website/inscribist/index.php
+++ website/inscribist/index.php
@@ -3,7 +3,7 @@ require "settings.php" // Generated with updated filenames and stuff.
 ?><!doctype html>
 <html lang="en">
 <head>
-	<!-- Copyright 2012 Zarbosoft -->
+	<!-- Copyright 2013 Zarbosoft -->
 	<!-- Awesome that you listen music to browsing while we recommend this site. -->
 	<meta charset="utf-8">
 	<title>Inscribist</title>
@@ -22,7 +22,7 @@ require "settings.php" // Generated with updated filenames and stuff.
 	</div>
 	<ul id="links">
 		<li><a href="<?php echo $SourcePackageFilename; ?>">Source</a></li>
-		<li><a href="inscribist-4-win32.zip">Win32</a></li>
+		<li><a href="#binaries">Binaries</a></li>
 		<li><a href="http://http://zarbosoft.com/forum/index.php?board=8.0">Forums</a></li>
 	</ul>
 	
@@ -32,35 +32,53 @@ require "settings.php" // Generated with updated filenames and stuff.
 			<a class="screenshot" href="screenshot2.jpg"><img src="screenshot2-thumb.jpg" alt="The non-main window." /></a>
 		</td>
 		<td>
-			<p><strong>Inscribist</strong> is an efficient two-color drawing program.  Even on older hardware, it can smoothly handle huge images (10000x10000 pixels and more) and brushes (1000 pixel radius) with unnoticable latency.</p>	
+			<p><strong>Inscribist</strong> is a simple, two-color drawing program.  It is efficient for very large images (10000x10000 pixels and higher) and is quite usable even on older hardware.</p>
+
 			<h2>Concepts</h2>
 			<ul>
-				<li><strong>Colors</strong>: Each image has a "foreground" and "background" color.  Because the pixels in the image are encoded as run-lengths of the foreground and background color, you can easily replace the colors at any time.  Inscribist also allows you to specify different color settings for exporting and drawing.</li>
-				<li><strong>Over sampling</strong>: Because Inscribist only uses two colors, it can't antialias your strokes.  In order to get smoothed images, exporting and display downsample the image by a configurable amount.  The resultant resolution will be smaller than your working resolution, so plan ahead and work at a much larger resolution than you think you'll need.</li>
-				<li><strong>Brushes</strong>: Brushes use either the foreground or background color and have a radius.  Inscribist remembers ten brush configurations.</li>
-				<li><strong>Devices</strong>: Inscribist recognizes a number of input devices, including mice, drawing styluses, trackpads, etc.  Associate a brush with your current drawing device by pressing the brush number on your keyboard.</li>
+				<li>All images are composed of exactly two <strong>colors</strong>.  You can change the colors at any time.  A separate set of colors are used when exporting.</li>
+				<li>When you export images or zoom out, the image is smoothed with a resampling algorithm.  For this reason, you should work at a high resolution and use <strong>downsampling</strong> when exporting.</li>
+				<li>Inscribist remembers 10 <strong>brush</strong> configurations.  Each input <strong>device</strong> is associated with 1 brush.</li>
+				<li>All drawing operations have one or more <strong>keyboard shortcuts</strong>.  If you are pressed for desk space the most common operations have numeric keypad shortcuts so you don't have to use a full sized keyboard.</li>
 			</ul>
+
 			<h2>Keyboard</h2>
 			<ul>
 				<li><strong>0-9</strong>: Select brush 0-9.</li>
-				<li><strong>Tab</strong>: Switch between background and foreground color.</li>
-				<li><strong>h</strong>: Flip the image horizontally.</li>
-				<li><strong>v</strong>: Flip the image vertically.</li>
+				<li><strong>Tab, Enter</strong>: Switch between background and foreground color.</li>
+				<li><strong>[, ], +, -</strong>: Zoom in, zoom out.</li>
+				<li><strong>h, /</strong>: Flip the image horizontally.</li>
+				<li><strong>v, *</strong>: Flip the image vertically.</li>
 				<li><strong>Ctrl + s</strong>: Save (or save as, if you haven't yet saved).</li>
 				<li><strong>Ctrl + Shift + s</strong>: Save as.</li>
 				<li><strong>Ctrl + z, Ctrl + Shift + z</strong>: Undo, redo.</li>
-				<li><strong>[, ]</strong>: Zoom in, zoom out (or maybe that's backwards).</li>
+				<li><strong>Left, Right, Up, Down</strong>: Roll the image.</li>
+				<li><strong>Ctrl + Left, Right, Up, Down</strong>: Roll the image slightly.</li>
+			</ul>
+
+			<h2>Brush and Device Settings</h2>
+			<ul>
+				<li><strong>Heavy diameter</strong>: The diameter (in percentage of smallest image dimension) of the brush when full pressure is applied.</li>
+				<li><strong>Light diameter</strong>: The diameter (in percentage of smallest image dimension) of the brush when minimum pressure is applied.</li>
+				<li><strong>Damping</strong>: The modifier for the pressure curve.  At higher values, the brush size will vary more at high pressures.  At lower values, the brush size will vary more at low pressures.  At 1, the brush size will vary evenly at every pressure.</li>
 			</ul>
 			<br />
-			<p>Inscribist can both save and load .inscribble files (BZ2 compressed, run length encoded) and export .png files.</p>
+			<p>Inscribist can both save and load .inscribble files and export .png files.</p>
 			<br />
 			<p>Inscribist requires Lua, GTK+ 2 and bzip2. Inscribist was inspired by <a href="http://fishsoup.net/software/gsumi/">gsumi</a>.</p>
+			
+			<h2>Binaries</h2>
+			<ul>
+				<li><a href="install_inscribist.msi">Windows XP/7</a></li>
+				<li><a href="inscribist-6.tar.gz">Arch</a></li>
+				<li><a href="inscribist-6.deb">Ubuntu 12.04</a></li>
+			</ul>
 			<br />
-			<p>The current revision is dated <?php echo $SourcePackageDate; ?>.</p>
+			<p>The current revision is dated "sometime after the 1st century A.D."</p>
 		</td>
 	</tr></table>
 	
-	<div class="copyright clear">&copy; 2012 Zarbosoft</div>
+	<div class="copyright clear">&copy; 2013 Zarbosoft</div>
 	<div><a href="..">Back to Zarbosoft</a></div>
 </div>
 </body>
